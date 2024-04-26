772b9e1822b1e07558a0dbdf8743186459ce445c
Luis Alberto Bolaños Salazar <68343185+albertbolanoss@users.noreply.github.com>
2024-04-25 20:08:53 -0500

    Run jar with spring boot

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,8 +3,8 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp codedeploystack-webappdeploymentbucket-09pf6atv8xuw/aws-shopping-car-1.0-SNAPSHOT.war /usr/local/tomcat10/webapps/aws-shopping-car-1.0-SNAPSHOT.war
-
+#aws s3 cp s3://codedeploystack-webappdeploymentbucket-09pf6atv8xuw/aws-shopping-car-1.0-SNAPSHOT.war /usr/local/tomcat10/webapps/aws-shopping-car-1.0-SNAPSHOT.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-09pf6atv8xuw/aws-shopping-car-1.0-SNAPSHOT.jar /usr/local/tomcat10/webapps/aws-shopping-car-1.0-SNAPSHOT.jar
 
 # Ensure the ownership permissions are correct.
 chown -R tomcat:tomcat /usr/local/tomcat10/webapps
